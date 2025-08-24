1594ae95b17964f72d117ee01df7acdeb956dd26
Oksana (Kes) Milenina <OkMilenina@yandex.ru>
2025-08-24 15:34:20 +0300

    objects homeworks

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,34 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    if(!this.hasOwnProperty("marks")) {
+        console.log("Невозможно добавить новые оценки");
+        return false;
+    }
+    this.marks = [...this.marks, ...marks];
+    return true;
 }
 
 Student.prototype.getAverage = function () {
-  
+    if(!this.hasOwnProperty("marks") || this.marks.length == 0) return 0;
+    let sum = this.marks.reduce(
+        (sum, next) => sum + next,
+        0
+    );
+    return sum / this.marks.length;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
 }
